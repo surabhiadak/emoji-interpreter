fe6b4a3d0ba2243eed03502aede61c17a59a97b1
Surabhi Adak <surabhiadak8@gmail.com>
2021-01-21 05:00:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "😊": "smiling",
+  "😀": "grinning face",
+  "😂": "Face With Tears of Joy",
+  "😃": "Smiling Face With Open Mouth",
+  "😍": "Smiling Face With Heart-Eyes",
+  "😘": "Face Blowing a Kiss",
+  "😇": "Smiling Face With Halo",
+  "😉": "wink",
+  "😐": "neutral face",
+  "😌": "relieved face",
+  "😟": "worried",
+  "😳": "disbelief",
+  "😒": "annoyance",
+  "😔": "dissatisfaction and frustration",
+  "😕": "Confused",
+  "😣": "struggling",
+  "😖": "extremely dismayed and bewildered",
+  "😫": "tired",
+  "😩": "weary face",
+  "😢": "crying face",
+  "😭": "loudly crying face",
+  "😤": "Face With Look of Triumph",
+  "🤬": "cursing",
+  "😠": "Angry Face",
+  "😡": "Pouting Face",
+  "😷": "masked face",
+  "😵": "dizzy",
+  "😯": "Speechless Face",
+  "😦": "Frowning Face With Open Mouth",
+  "😮": "surprised",
+  "😲": "Astonished Face",
+  "🧐": "Face with monocle",
+  "🤯": "Mind blown",
+  "🤓": "Nerd face",
+  "❤️": "love"
+};
+
+const emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState(
+    "Click on the emoji below or search for emoji 👆"
+  );
+
+  function emojiInputHandler(event) {
+    const userInput = event.target.value;
+
+    if (userInput in emojiDictionary) {
+      setMeaning(emojiDictionary[userInput]);
+    } else {
+      setMeaning("Failure to recognise this emoji");
+    }
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <header>
+        <h1>Find your Emoji🔎 </h1>
+      </header>
+      <input onChange={emojiInputHandler}></input>
+
+      <h2> {meaning} </h2>
+
+      <h3> Emojis We Know </h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
